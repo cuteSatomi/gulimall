a63720167f21619e3a6dae6bd098f7d81ec1c2f1
cuteSatomi <hdsomedezzx@gmail.com>
2021-06-08 22:22:32 +0800

    整合rabbitmq完成

--- gulimall-order/src/test/java/com/zzx/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/zzx/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,19 @@
 package com.zzx.gulimall.order;
 
+import com.zzx.gulimall.order.entity.OrderReturnReasonEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -17,11 +23,39 @@ public class GulimallOrderApplicationTests {
     @Autowired
     private AmqpAdmin amqpAdmin;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void sendMessage() {
+        // String msg = "Hello World!";
+        OrderReturnReasonEntity reasonEntity = new OrderReturnReasonEntity();
+        reasonEntity.setCreateTime(new Date());
+        reasonEntity.setId(1L);
+        reasonEntity.setName("嘻嘻嘻");
+        rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", reasonEntity);
+        log.info("消息发送成功:{}", reasonEntity);
+    }
+
     @Test
     public void createExchange() {
-        DirectExchange directExchange = new DirectExchange("hello-java-exchange",true,false);
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange", true, false);
         amqpAdmin.declareExchange(directExchange);
         log.info("hello-java-exchange创建成功");
+    }
+
+    @Test
+    public void createQueue() {
+        Queue queue = new Queue("hello-java-queue", true, false, false);
+        amqpAdmin.declareQueue(queue);
+        log.info("hello-java-queue创建成功");
+    }
+
+    @Test
+    public void bindingExchange() {
+        Binding binding = new Binding("hello-java-queue",
+                Binding.DestinationType.QUEUE, "hello-java-exchange", "hello.java", null);
+        amqpAdmin.declareBinding(binding);
     }
 
 }
